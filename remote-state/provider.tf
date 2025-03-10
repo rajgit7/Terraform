9983a9685e133cc5d32f539c3b4f571bf2fcf883
rajgit7 <rajprudvin@gmail.com>
2025-03-10 20:00:15 +0530

    adding topics of terraform

--- remote-state/provider.tf
+++ remote-state/provider.tf
@@ -5,6 +5,13 @@ terraform {
       version = "5.90.0"
     }
   }
+
+   backend "s3" {
+    bucket = "raj-remotestate"
+    key    = "remote-state-demo"
+    region = "us-east-1"
+    dynamodb_table = "raj-locking"
+  }
 }
 
 provider "aws" {
